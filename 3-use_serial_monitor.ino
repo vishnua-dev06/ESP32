d5843858d69ab8606ce122a86c66a329ada9e73f
Vishnu A <137207623+vishnua-dev06@users.noreply.github.com>
2026-06-28 23:52:43 +0530

    Update 3-use_serial_monitor.ino

--- 3-use_serial_monitor.ino
+++ 3-use_serial_monitor.ino
@@ -1,1 +1,16 @@
-
+int ledPin = 2;
+String cmd;
+void setup(){
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+}
+void loop(){
+    if(Serial.available()){
+      cmd = Serial.readString();
+    }
+    if(cmd == "ON")
+       digitalWrite(ledPin, HIGH);
+    else if(cmd=="off")
+       digitalWrite(ledPin, LOW);
+delay(200);
+}
