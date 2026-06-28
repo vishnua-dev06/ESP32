cab9cf553e43ebf01626fe696d12fd6fbdf5b887
Vishnu A <137207623+vishnua-dev06@users.noreply.github.com>
2026-06-28 23:36:34 +0530

    Create touchsensor_pins.ino

---
+++ touchsensor_pins.ino
@@ -0,0 +1,13 @@
+int ledPin = 2;
+
+void setup(){
+    Serial.begin(115200);
+    pinMode(ledPin, OUTPUT);
+}
+void loop(){
+  if( touchRead(T0)<200)
+    digitalWrite(ledPin, HIGH);
+  else
+    digitalWrite(ledPin, LOW);
+delay(200);
+}
